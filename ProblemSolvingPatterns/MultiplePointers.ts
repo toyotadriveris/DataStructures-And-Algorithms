0269762a5f8221b138b6b6aaa730b73879b8fa27
toyotadriver <polaemil@gmail.com>
2025-06-27 01:01:56 +0300

    "unlucky"

--- ProblemSolvingPatterns/MultiplePointers.ts
+++ ProblemSolvingPatterns/MultiplePointers.ts
@@ -66,4 +66,44 @@ function areThereDuplicates(): boolean {
   return false;
 }
 
-console.log(areThereDuplicates(1, 2, 2));
+// console.log(areThereDuplicates(1, 2, 2));
+function averagePair(arr: number[], avg: number): boolean {
+  if (!arr || arr.length === 0) return false;
+
+  let condition = avg * 2;
+
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    if (arr[left] + arr[right] === condition) {
+      return true;
+    } else if (arr[left] + arr[right] > condition) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return false;
+}
+
+// console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8));
+
+function isSubsequence(str1: string, str2: string): boolean {
+  let result = 0;
+  let j = 0;
+  for (let i = 0; i < str2.length; i++) {
+    if (result >= 2) return true;
+    // console.log(str1[i]);
+    // console.log(str2[j]);
+
+    if (str2[i] === str1[j]) {
+      result += 1;
+      j += 1;
+    }
+    // console.log(result);
+  }
+  return false;
+}
+console.log(isSubsequence("abc", "acb"));
